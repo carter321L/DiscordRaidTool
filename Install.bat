3bc6a942cd6d3c57907927515e3c5fdc527ec8dc
carter321L <156473816+carter321L@users.noreply.github.com>
2024-01-26 09:20:18 -0500

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,26 @@
+pip install os
+pip install requests
+pip install tls_client
+pip install datetime
+pip install sys
+pip install hashlib
+pip install threading
+pip install websocket
+pip install websocket_client
+pip install json
+pip install time
+pip install httpx
+pip install typing
+pip install subprocess
+pip install colorama
+pip install emoji
+pip install fade
+pip install pylibcheck
+pip installbeautifulsoup4
+pip install undetected_chromedriver
+pip install tinyaes
+pip install keyboard
+pip install PIL-Tools
+pip install pycryptodome
+pip install colorama
+pip install pefile
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+python massdm.py
+pause
